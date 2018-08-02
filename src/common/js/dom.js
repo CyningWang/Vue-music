4dab441f207a3c01eff5fb51937b016107296546
CyningWang <1652545601@qq.com>
2018-08-02 23:52:19 +0800

    歌手Tab页完成

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -12,3 +12,14 @@ export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
+
+// 获取vue标签的自定义属性，形如：[:data-key=val]
+export function elData(el, name, val) {
+  const prefix = 'data-'
+  name = prefix + name
+  if (val) {
+    return parseFloat(el.setAttribute(name, val))
+  } else {
+    return parseFloat(~~el.getAttribute(name))
+  }
+}
